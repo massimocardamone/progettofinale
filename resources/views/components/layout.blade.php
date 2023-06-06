759bdf6d2ca5d60947704fa1516012214ae209f6
StefanoCossu <cossu.stefano1984@gmail.com>
2023-06-06 11:09:43 +0200

    Components inseriti

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{$docTitle}}</title>
     @vite(['resources/css/app.css'])
 </head>
 <body>
-    
+    <x-header title="{{$title}}"/>
+    {{$slot}}
     @vite(['resources/js/app.js'])
 </body>
 </html>
