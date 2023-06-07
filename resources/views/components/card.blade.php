2eadff51563c4cca0786125e53118ac5d65d918e
walter ladislao <walterladislao00@gmail.com>
2023-06-07 11:39:19 +0200

    inserita card

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,22 @@
+<div class="card mycard" style="width: 18rem;">
+    <div class="position-relative">
+        <img class="cardimg" src="https://picsum.photos/id/237/287/300" alt="">
+        <div class="cardcategory">
+            <!-- qui va il link alla pagina con tutti i prodotti della stessa categoria -->
+            <a href="">categoria</a>
+        </div>
+    </div>
+    
+    <div class="m-2 mt-0">
+       
+        <div class="cardtitle mt-2">
+            <h3>Titolo prodotto</h3>
+        </div>
+        <div class="cardprice">
+            <h4>prezzo</h4>
+        </div>
+        <div class="carddescription mt-1">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem commodi tempora sunt itaque laborum quas nisi tenetur, ipsum, optio, atque omnis excepturi consequatur error quasi a odio repellat voluptas fuga?</p>
+        </div>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <x-layout docTitle="Homepage" title="Homepage">
 
-
+<x-card></x-card>
+<x-card></x-card>
 
 
 
