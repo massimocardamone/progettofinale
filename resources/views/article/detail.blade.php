fd93f48cfb0ba9bccb37503f9f9782725c3efdd9
walter ladislao <walterladislao00@gmail.com>
2023-06-07 20:42:39 +0200

    vista dettaglio front-end implementato

--- resources/views/article/detail.blade.php
+++ resources/views/article/detail.blade.php
@@ -1,11 +1,8 @@
 <x-layout docTitle="detail" title="{{$article->name}}">
      
-<div class="container-fluid">
+<div class="container mt-3">
     <div class="row justify-content-center ">
-        <div class=" col-12 col-md-6 ">
-            <img class="img-fluid" src="{{Storage::url($article->img)}}" alt="">
-        </div>
-        <div class="col-12 col-md-6  d-flex align-items-center justify-content-center ">
+        <div class="col-12 col-md-6 d-flex align-items-center justify-content-center bg-primary">
             <ul class="">
                 <li class="list-group text-light">Nome: {{$article->name}}</li>
                 <li class="list-group text-light">Prezzo:€{{$article->price}}</li>
@@ -14,7 +11,23 @@
                 <a href="{{route('home')}}" class="btn btn-success my-3">torna indietro</a>
             </ul>
         </div>
+        <div class=" col-12 col-md-6  coldetdx">
+            <div class="coldetdxd ">
+                <img class="img-fluid imgdet" src="{{Storage::url($article->img)}}" alt="">
+            </div>            
+        </div>
         
-    </div>
+        <div class="col-12 col-md-6  coldetsx">
+            <div class="coldetsxd ">
+                <img class="img-fluid imgdet" src="{{Storage::url($article->img)}}" alt="">
+            </div>            
+        </div>
+
+        <div class="col-12 col-md-6  coldetdx">
+            <div class="coldetdxd ">
+                <img class="img-fluid imgdet" src="{{Storage::url($article->img)}}" alt="">
+            </div>            
+        </div>        
+    </div>    
 </div>
 </x-layout>
