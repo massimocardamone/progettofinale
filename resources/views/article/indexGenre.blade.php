e760be038eabf27dcf932f814e2517fd6755d303
StefanoCossu <cossu.stefano1984@gmail.com>
2023-06-08 09:58:50 +0200

    h2 non h1

--- resources/views/article/indexGenre.blade.php
+++ resources/views/article/indexGenre.blade.php
@@ -22,7 +22,7 @@
     <div class="container my-5">
         <div class="row">
             <div class="col-12">
-                <h1 class="text-center">Nessun Articolo per la categoria</h1> 
+                <h2 class="text-center">Nessun Articolo per la categoria</h2> 
             </div>
         </div>
     </div>
