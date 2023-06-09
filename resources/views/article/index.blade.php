936906288f00b0463d028385125805468f1f1b10
Davide Ieria <davideieria99@gmail.com>
2023-06-09 10:00:59 +0200

    aggiustato le categorie del form

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -4,7 +4,7 @@
         {{-- LISTA CATEGORIE --}}
         <div class="row justify-content-evenly py-2 mysection">
             @foreach ($genres as $genre )
-            <div class="col-4 col-md-2 mx-1 d-flex justify-content-center">
+            <div class="col-4 col-sm-4 col-md-2 mx-1 d-flex justify-content-center">
                 <a class="btn" href="{{route('show_category', compact('genre', 'genres'))}}">
                     
                     <button class="mybtn">{{$genre->genre}}</button>
@@ -13,10 +13,11 @@
             @endforeach
         </div>
     </div>
+    {{-- LISTA CARD --}}
     <div class="container mysection">
         <div class="row">
             @foreach ($articles as $article)
-            <div class="col-12 col-md-3">
+            <div class="col-12 col-sm-8 col-md-3 mx-sm-auto">
                 <x-card :article="$article"/>
             </div>
             @endforeach
@@ -25,5 +26,4 @@
             {{$articles->links()}}
         </div>
     </div>
-    
 </x-layout>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,22 +1,21 @@
 <x-layout docTitle="Login" title="Effettua il Login">
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <form method="POST" action="{{route('login')}}">
+            <form method="POST" action="{{ route('login') }}">
                 @csrf
                 <div class="mb-3">
                     <label for="userMail" class="form-label">Email utente</label>
-                    <input type="email" class="form-control" name="email" id="userMail"> 
+                    <input type="email" class="form-control @error('email') is invalid @enderror" name="email"
+                        id="userMail">
                 </div>
-                <div class="mb-3">    
+                <div class="mb-3">
                     <label for="userPassword" class="form-label">Password</label>
-                    <input type="password" name="password" class="form-control" id="userPassword">
+                    <input type="password" name="password" class="form-control @error('email') is invalid @enderror" id="userPassword">
                 </div>
                 <div class="mb-3 form-check">
                     <button type="submit" class="btn btn-primary">Submit</button>
-                </div>    
+                </div>
             </form>
         </div>
     </div>
 </x-layout>
-
-
