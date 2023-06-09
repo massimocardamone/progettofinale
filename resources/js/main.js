dc92766ae8bc38e0ad6135158adf45f8f24b3cea
StefanoCossu <cossu.stefano1984@gmail.com>
2023-06-09 12:38:35 +0200

    hidden su button non in uso

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,12 +1,16 @@
 let link = document.querySelectorAll('.page-link')
 let liLink=document.querySelectorAll('.page-item')
-liLink.forEach(element=>{
-    element.classList.remove('active');
-    element.classList.remove('disabled')
-})
 link.forEach(element => {
     element.classList.add('mybtn');
     element.classList.add('mx-3');
 });
-link[0].innerHTML='Precedente';
-link[1].innerHTML='Successivo';
+liLink.forEach(element=>{
+    element.classList.remove('active');
+    element.classList.remove('disabled')
+    if (element.ariaDisabled) {
+        element.classList.add('invisible')
+    }
+})
+
+link[0].innerHTML='<< Precedente';
+link[1].innerHTML='Successivo >>';
