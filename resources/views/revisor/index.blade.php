ac68124fdd0ca32f6d593296922474e8d4a2679a
Davide Ieria <davideieria99@gmail.com>
2023-06-09 11:47:42 +0200

    merge

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -1,16 +1,5 @@
 <x-layout docTitle="home" title="revisiona">
 
-    {{-- @dd($article_to_check->all()) --}}
-    <div class="container">
-        <div class="row">
-            @foreach ($article_to_check as $article )
-            <x-card :article="$article"/>
-                
-            @endforeach
-            <div class="col-12 col-md-3">
-            </div>
-        </div>
-    </div>
 
 
 </x-layout>
