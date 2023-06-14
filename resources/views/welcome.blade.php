3811863b59d2775f62c5071a906a285795be0640
walter ladislao <walterladislao00@gmail.com>
2023-06-14 18:26:27 +0200

    oke oke

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <x-layout docTitle="Homepage" title="C o l i c a S t o r e">
   
   <div class="container mymotto d-flex justify-content-center">
@@ -6,16 +5,6 @@
       <div class="col-12 d-flex  justify-content-center">
         <h2 class="fst-italic">Prezzi... da mal di pancia!</h2>
       </div>
-=======
-<x-layout docTitle="Homepage" title="C o l i c a S t o r e" animate="animate__backInDown">
-
-  <x-number-section artNum='{{$artNum}}' userNum='{{$userNum}}'/> 
-
-<div class="container mysezione11">
-  <div class="row">
-    <div class="col-12">
-      <h3 class="fst-italic">I nostri nuovi prodotti!</h3>
->>>>>>> 9c8b22b0bfaee47d8b07fa2b6f72e17f829c6b1c
     </div>
   </div>
   
@@ -31,13 +20,8 @@
     <div class="row bg-danger justify-content-between">
       <div class="col-12 col-md-8 bg-warning d-flex">
         @foreach ($articles as $article)
-<<<<<<< HEAD
-        <div class="col-12 col-md-3">     
-          <x-card :article='$article'/>      
-=======
         <div class="col-12 col-md-4">     
           <x-card :article='$article' animate="animate__fadeIn"/>      
->>>>>>> 9c8b22b0bfaee47d8b07fa2b6f72e17f829c6b1c
         </div>
         @endforeach        
       </div>
