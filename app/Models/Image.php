f83576c422c18e635a852085678938a6ad5d810b
StefanoCossu <cossu.stefano1984@gmail.com>
2023-06-14 11:52:35 +0200

    user 6 in corso KTM

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -2,17 +2,34 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use App\Models\Article;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Image extends Model
 {
     use HasFactory;
 
     protected $fillable = ['path'];
+
     public function article()
     {
         return $this->belongsTo(Article::class);
     }
+    public static function getUrlByFilePath($filePath,$w=null,$h=null){
+        if(!$w && !$h){
+            return Storage::url($filePath);
+        }
+        $path= dirname($filePath);
+        $filename = basename($filePath);
+        $file = "{$path}/crop_{$w}x{$h}_{$filename}";
+        return Storage::url($file);
+    }
+
+    public function getUrl($w=null,$h=null){
+        return Image::getUrlByFilePath($this->path, $w,$h);
+    }
+
 }
