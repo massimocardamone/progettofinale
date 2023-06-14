dfdcbb1aca0febc113f40241cee36d862358c759
StefanoCossu <cossu.stefano1984@gmail.com>
2023-06-14 20:04:57 +0200

    piglia per il culo

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,10 @@ import '@fortawesome/fontawesome-free/js/all.js';
 import './main';
 import 'animate.css';
 
-// import '/node_modules/swiper/swiper-bundle.min.js';
-// import '/node_modules/swiper/modules/effect-cards/effect-cards.js';
-// import '/node_modules/swiper/modules/navigation/navigation.js';
-// import '/node_modules/swiper/modules/pagination/pagination.js';
+import '/node_modules/swiper/swiper-bundle.min.js';
+import '/node_modules/swiper/modules/effect-cards/effect-cards.js';
+import '/node_modules/swiper/modules/navigation/navigation.js';
+import '/node_modules/swiper/modules/pagination/pagination.js';
 
 import Swiper from "swiper";
 import { Autoplay, EffectCards, Navigation, Pagination } from "swiper";
